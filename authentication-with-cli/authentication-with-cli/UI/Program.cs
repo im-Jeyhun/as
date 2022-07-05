c74a049a98877325b4b0afbc64ee3b4c3fee687c
Ceyhun <contact.ceyhun@gmail.com>
2022-07-05 12:06:18 +0400

    initial commit

---
+++ authentication-with-cli/authentication-with-cli/UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace authentication_with_cli.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
